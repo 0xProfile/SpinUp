b07524602a99c9e598b668986a581b01a2333249
Kehan Wang <sbtkehanwang@gmail.com>
2023-08-10 00:55:31 -0700

    Add AWS SDK support

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
+import AWS from 'aws-sdk';
+
 
 const Home = () => {
+
+  const [accessKeyId, setAccessKeyId] = useState('');
+  const [secretAccessKey, setSecretAccessKey] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can perform any logic with the collected credentials
+    console.log('Access Key ID:', accessKeyId);
+    console.log('Secret Access Key:', secretAccessKey);
+    const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+    var instanceParams = {
+      ImageId: 'ami-04e35eeae7a7c5883',
+      InstanceType: 't2.micro',
+      MinCount: 1,
+      MaxCount: 1
+    };
+
+    // Create a promise on an EC2 service object
+    var instancePromise = new AWS.EC2({
+      apiVersion: '2016-11-15',
+      region: 'us-west-2',
+      credentials: {
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey
+      }
+    }).runInstances(instanceParams).promise();
+
+    // Handle promise's fulfilled/rejected states
+    instancePromise.then(
+      function (data) {
+        console.log(data);
+        var instanceId = data.Instances[0].InstanceId;
+        console.log("Created instance", instanceId);
+      }).catch(
+        function (err) {
+          console.error(err, err.stack);
+        });
+  };
+
   return (
     <div>
       <div className="flex items-center justify-between mb-10">
@@ -74,6 +114,70 @@ const Home = () => {
             </Menu>
           </div>
         </div>
+      </div>
+
+      <div className="justify-center items-center">
+        <form onSubmit={handleSubmit} className="bg-gray-800 p-8 rounded-lg shadow-md">
+          <h2 className="text-2xl mb-4 text-white">AWS Credentials</h2>
+          <div className="mb-4">
+            <label className="block font-medium mb-2 text-white" htmlFor="accessKeyId">Access Key ID</label>
+            <input
+              type="text"
+              id="accessKeyId"
+              className="w-full border rounded-md p-2 bg-gray-700 text-white"
+              value={accessKeyId}
+              onChange={(e) => setAccessKeyId(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-medium mb-2 text-white" htmlFor="secretAccessKey">Secret Access Key</label>
+            <input
+              type="password"
+              id="secretAccessKey"
+              className="w-full border rounded-md p-2 bg-gray-700 text-white"
+              value={secretAccessKey}
+              onChange={(e) => setSecretAccessKey(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-primary py-2 px-4 rounded-md hover:bg-secondary-900 transition-colors"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="justify-center items-center">
+        <form onSubmit={handleSubmit} className="bg-gray-800 p-8 rounded-lg shadow-md">
+          <h2 className="text-2xl mb-4 text-white">Azure Credentials</h2>
+          <div className="mb-4">
+            <label className="block font-medium mb-2 text-white" htmlFor="accessKeyId">Access Key ID</label>
+            <input
+              type="text"
+              id="accessKeyId"
+              className="w-full border rounded-md p-2 bg-gray-700 text-white"
+              value={accessKeyId}
+              onChange={(e) => setAccessKeyId(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block font-medium mb-2 text-white" htmlFor="secretAccessKey">Secret Access Key</label>
+            <input
+              type="password"
+              id="secretAccessKey"
+              className="w-full border rounded-md p-2 bg-gray-700 text-white"
+              value={secretAccessKey}
+              onChange={(e) => setSecretAccessKey(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-primary py-2 px-4 rounded-md hover:bg-secondary-900 transition-colors"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
